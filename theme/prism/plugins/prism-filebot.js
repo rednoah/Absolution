47ddff0c32888847f93a52c260052e61e8917a92
Reinhard Pointner <rednoah@filebot.net>
2024-10-17 14:16:02 +0700

    Add it global variable (to better highlight closures)

--- theme/prism/plugins/prism-filebot.js
+++ theme/prism/plugins/prism-filebot.js
@@ -53,7 +53,7 @@
 		}
 	};
 	Prism.languages.groovy.global = {
-		pattern: /\b(?:null|none|any|allOf|concat|quote|toJson|include|text|csv|lines|xml|json|html|XML)\b/,
+		pattern: /\b(?:null|it|none|any|allOf|concat|quote|toJson|include|text|csv|lines|xml|json|html|XML)\b/,
 		alias: 'function'
 	};
 	Prism.languages.groovy.dgm = {
